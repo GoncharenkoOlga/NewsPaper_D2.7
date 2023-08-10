c5570eebe58bfca9cd86c01631ba4668efa9ff93
GoncharenkoOlga <sweetberry2004@bk.ru>
2023-08-10 11:32:28 +0200

    Homework_+D5_start_v3

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMix
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, get_object_or_404
 from datetime import datetime, timedelta
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 from .models import Post, Author
 from .filters import PostFilter
 from .forms import PostForm
@@ -16,9 +16,17 @@ from django.views import View
 from .tasks import hello, printer
 
 
+class IndexView(View):
+    def get(self, request):
+        printer.apply_async([10],
+                            eta = datetime.now() + timedelta(seconds=5))
+        hello.delay()
+        return HttpResponse('Hello!')
+
+
 class PostsList(ListView):
     model = Post
-    ordering = 'posting_time'  # от более свежей к самой старой
+    ordering = '-posting_time'  # от более свежей к самой старой
     template_name = 'posts.html'
     context_object_name = 'posts'
     paginate_by = 3
@@ -96,3 +104,29 @@ class ArticleCreate(CreateView, PermissionRequiredMixin):
         post.choice_field = 'article'
         post.author = Author.objects.get(user=self.request.user)
         return super().form_valid(form)
+
+
+class CategoryListView(ListView):
+    model = Post
+    template_name = 'category_list.html'
+    context_object_name = 'category_news_list'
+
+    def get_queryset(self):
+        self.category = get_object_or_404(Category, id = self.kwargs['pk'])
+        queryset = Post.objects.filter(category = self.category).order_by('-posting_time')
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_not_subscriber'] = self.request.user not in self.category.subscribers.all()
+        context['category'] = self.category
+        return context
+
+
+@login_required
+def subscribe(request, pk):
+    user = request.user
+    category = Category.objects.get(id=pk)
+    category.subscribers.add(user)
+    message = 'Вы успешно подписались на рассылку категории'
+    return render(request, 'subscribe.html', {'category': category, 'message': message})
